465d5499ab9f7613b20f80d1a96395a6f692c438
Nancy Badillo <n_badillo@csu.fullerton.edu>
2020-11-20 22:00:11 -0800

    Add search() and css to buttons

--- main.js
+++ main.js
@@ -39,6 +39,30 @@ function delete_element(){
     display_array();
 }
 
+function search_element(){
+    element_found = false;
+    element_being_searched = document.getElementById("inputField").value;
+    document.getElementById("inputField").value = ""; // sets the inputField to blank
+    
+    // scan to see if it is inside of the array
+    for(var index = 0; index < array.length; index++){
+        if(array[index] == element_being_searched){
+            element_found = true;
+            array.splice(index, 1);
+            move = "search(" + element_being_searched + ") => true" + "<br/>";
+            console.log(move);
+            moves_done.push(move);
+        }
+    }
+    if(element_found == false){
+        document.getElementById("inputField").value = "";
+        alert("Item not found!");
+    }
+
+    display_moves();
+    display_array();
+}
+
 function display_array(){
     if(array.length == 0){
         document.getElementById("Result").innerHTML = "Array is empty!";
@@ -49,5 +73,6 @@ function display_array(){
 }
 
 function display_moves(){
+    // TODO: Redo this so that there is no , and that we do not need </br> in the moves_done.push();
     document.getElementById("displayMoves").innerHTML = moves_done;
 }
