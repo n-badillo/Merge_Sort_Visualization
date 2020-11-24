b64bc8e36b3b377e1dfd02c5d7b30fd14395cabf
Nancy Badillo <n_badillo@csu.fullerton.edu>
2020-11-23 22:37:11 -0800

    Create react app and have randomly generated circle nodes

---
+++ red_black_tree/src/App.js
@@ -0,0 +1,12 @@
+import BinarySearchVisualizer from './BinarySearchVisualizer/BinarySearchVisualizer';
+import './App.css';
+
+function App() {
+  return (
+  <div class="App">
+    <BinarySearchVisualizer></BinarySearchVisualizer>
+  </div>
+  );
+}
+
+export default App;
